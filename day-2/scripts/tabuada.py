c80754608acd594d26b0a8035539c9e2d70f7aaa
Bruno Nascimento <bruno.ol.nascimento@gmail.com>
2024-12-09 20:49:05 +0000

    chore(lc-009): adding tabuada.py version 2

--- day-2/scripts/tabuada.py
+++ day-2/scripts/tabuada.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python
 """Imprime a tabuada do 1 ao 10.
 
-Tabuada do 1
-1
-2
-3
+---Tabuada do 1---
+    
+    1 x 1 = 1
+    2 x 1 = 2
+    3 x 1 = 3
+
 ...
--------------
-Tabuada do 2
-2
-4
+##################
+---Tabuada do 2---
+
+    2 x 1 = 2
+    4 x 2 = 8
+
 ...
--------------
+##################
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Bruno Nascimento"
 
 #numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numeros = list(range(1, 11))
 
-for numero in numeros: 
-    print ("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("----------")
+for n1 in numeros: 
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+
+    print ("#" * 18)
