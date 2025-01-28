0607cea5ca07355f365c7a37ba0629238db7f48b
Bruno Nascimento <bruno.ol.nascimento@gmail.com>
2025-01-28 19:17:10 +0000

    chore(lc-026): add new files

---
+++ day-3/ebooks/3-6-exercicios-iteracoes-textos-inputs-arquivos-texto.md
@@ -0,0 +1,55 @@
+# Exercicios
+
+Agora vamos fazer uma série de exercícios de fixação.
+
+## Faça um programa que imprime os números pares de 1 a 200
+ex ```python numeros_pares.py```
+
+```python
+2
+4
+6
+8
+...
+```
+
+## Repete vogais
+
+Faça um programa que pede ao usuário que digite uma ou mais palavras e imprime cada uma das palavras com suas vogais duplicadas.
+
+ex:
+
+```python
+python repete_vogal.py
+'Digite uma palavra (ou enter para sair):' Python
+'Digite uma palavra (ou enter para sair):' Bruno
+'Digite uma palavra (ou enter para sair):' <enter>
+Pythoon
+Bruunoo
+```
+
+## Alarme de temperatura
+
+Faça um script que pergunta ao usuário qual a temperatura atual e o indice de umidade do ar sendo que caso será exibida uma mensagem de alerta dependendo das condições:
+
+temp maior 45: "ALERTA!!! 🥵 Perigo calor extremo"
+
+temp maior que 30 e temp vezes 3 for maior ou igual a umidade:
+
+    "ALERTA!!! 🥵♒ Perigo de calor úmido"
+
+temp entre 10 e 30: "😀 Normal"
+
+temp entre 0 e 10: "🥶 Frio"
+
+temp <0: "ALERTA!!! ⛄ Frio Extremo."
+
+ex:
+
+```python
+python3 temp.py 
+temperatura: 30
+umidade: 90
+... 
+"ALERTA!!! 🥵 Perigo calor extremo"
+````
---
+++ day-3/scripts/reserva.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+Faça um programa de terminal que exibe ao usuário uma lista dos quaertos
+disponíveis para alugar e o preço de cada quarto. Esta informação está 
+disponível em um arquivo de texto separado por vírgulas.
+
+`quartos.txt`
+# codigo, nome, preço
+1,Suite Master,500
+2,Quarto Familia,200
+3,Quarto Single,100
+4,Quarto Simples,50
+
+O programa pergunta ao usuário o nome, qual o número do quarto a ser
+reservado e a quantidade de dias. No final ele exibe o valor estimado
+a ser pago.
+
+O programa deve salvar esta escolha em outro arquivo contento as reservas.
+
+`reservas.txt`
+# cliente, quarto, dias
+Bruno,3,12
+
+Se outro usuário tentar reservar o mesmo quarto, o programa deve exibir uma
+mensagem informando que já está reservado.
+"""
+
+import sys
+import logging
+
+# Quartos ocupados - `ocupados.txt`
+ocupados = {}
+
+try:
+    for line in open("reservas.txt"):
+        nome, num_quarto, dias = line.strip().split(",")
+        ocupados[int(num_quarto)] = {
+            "nome": nome,
+            "dias": dias
+        }
+except FileNotFoundError:
+    logging.error("Arquivo `reservas.txt` não existe!")
+    sys.exit(1)
+
+# Quartos vazios - `arquivo.txt`
+quartos = {}
+
+try:
+    for line in open("quartos.txt"):
+        codigo, nome, preco = line.strip().split(",")
+        quartos[int(codigo)] = {
+            "nome": nome,
+            "preco": float(preco), # TODO: Decimal
+            "disponivel": False if int(codigo) in ocupados else True
+        }
+except FileNotFoundError:
+    logging.error("Arquivo `quartos.txt` não existe!")
+    sys.exit(1)
+
+if len(ocupados) == len(quartos):
+    print("Hotel lotado!")
+    sys.exit(1)
+
+nome = input("Nome do cliente: ").strip()
+print("Reserva Hotel Pythonico")
+print("-" * 40)
+print("Lista de quartos:")
+
+for codigo, dados in quartos.items():
+    nome_quarto = dados["nome"]
+    preco = dados["preco"]
+    disponivel = "⛔" if not dados["disponivel"] else "👍"
+    # disponivel = dados["disponivel"] and "👍" or "⛔"
+
+    # TODO: Substituir casa decimal por vírgula
+    print(f"{codigo} - {nome_quarto} - R$ {preco:.2f} - {disponivel}")
+
+print("-" * 40)
+
+try:
+    num_quarto = int(input("Escolha um quarto: ").strip())
+    if not quartos[num_quarto]["disponivel"]:
+        print(f"O Quarto {num_quarto} está ocupado")
+        sys.exit(1)
+except ValueError:
+    logging.error("Número inválido, insira apenas dígitos.")
+    sys.exit(1)
+except KeyError:
+    print(f"O quarto {num_quarto} não existe.")
+
+try:
+    dias = int(input("Quantos dias? ").strip())
+except ValueError:
+    logging.error("Número inválido, insira apenas dígitos.")
+    sys.exit(1)
+
+nome_quarto = quartos[num_quarto]["nome"]
+preco_quarto = quartos[num_quarto]["preco"]
+disponivel = quartos[num_quarto]["disponivel"]
+total = preco_quarto * dias
+
+with open("reservas.txt", "a") as file_:
+    file_.write(f"{nome},{num_quarto},{dias}\n")
+    # print(",".join([nome, str(num_quarto), str(dias)])) # Forma alternativa
+
+print(f"Olá {nome}, você escolheu o quarto `{nome_quarto}` e vai custar: R$ {total:.2f}")
